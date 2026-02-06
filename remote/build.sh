f5f2d189e81279146647f1ea612e66ba806787e1
Jesse B. Miller <jesse@jessebmiller.com>
2026-02-05 21:49:15 -0600

    update remote

--- remote/build.sh
+++ remote/build.sh
@@ -3,6 +3,10 @@
 set -e
 
 CFLAGS="-Wall -Wextra -O0 -g -lbunuel"
+SDL3PATH="../platform/vendor/SDL3-3.4.0/dist"
+CFLAGS="$CFLAGS -I$SDL3PATH/include"
+CFLAGS="$CFLAGS -L$SDL3PATH/lib64"
+CFLAGS="$CFLAGS -lSDL3"
 CFILES="main.c"
 
 gcc $CFILES $CFLAGS -o remote
--- remote/main.c
+++ remote/main.c
@@ -15,12 +15,8 @@ int main(int argc, char *argv[]) {
 	}
 	printf("\n");
 	
-	// TODO: write struct Window that is zero initialized
 	Window window = {0};
 	window.title = "/remote/";
-	// TODO: write bool open_window(Window window);
-	//       modifies window adding renderer, event queue, etc.
-	//       uses good defaults (remember changing defaults can require a version bump)
 	if(!open_window(&window)) {
 		error("Could not open window. Exiting.");
 		exit(-1);
@@ -28,34 +24,26 @@ int main(int argc, char *argv[]) {
 
 	bool running = true;
 	while(running) {
-		// TODO: write struct Event, zero initialized
 		Event event;
-		// TODO: write int take_event(Window *window, Event *event); returning the number of events ramaining in the queue
-		while(take_event(&window, &event)) {
+		while(event_take(&event)) {
 			debug("Got %s event", event_name(event));
 			switch(event.type) {
-			// TODO: make event type enum with DOs and DIDs to differentiate between intentions and occurences
 			case DO_QUIT: {
 				debug("Handling DO_QUIT");
 				running = false;
-			}
-			break;
+			} break;
 			case DID_WINDOW_RESIZE: {
-				// TODO: make sure take_event updates the window based on changes to underlying implementation
+				// TODO: make sure take_event updates the window based on changes to the raw window
 				debug("Window resized to %dx%d", window.width, window.height);
-			}
-			break;
-			default: {
-				// TODO: write char* event_name(EventType type);
+			} break;
+			default:
 				devln("Uhandled event %s", event_name(event));
 			}
+			// TODO: implement target delta or fps aware wait
 		}
-		// TODO: Write void clear(Window *window); That uses the window's set clear color (default black);
-		clear(&window);
-		// TODO: write bool present(Window *window, BackBuffer *buf); presents the buffer in the window
-		present(&window);
+		window_clear(&window);
+		window_present(&window);
 	}
-	// TODO: write bool close_window(Window window);
 	close_window(&window);
 	
 	return 0;
